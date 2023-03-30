6205834592211377ed539f60047df8421a971ff2
Ishwari Kamat <ishwarikmt7@gmail.com>
2023-03-31 01:19:36 +1100

    Authentication & Authorization using JWT Strategy.

--- server.js
+++ server.js
@@ -1,144 +1,51 @@
 const express= require("express");
-const res = require("express/lib/response");
+const jwt = require("jsonwebtoken");
 const app= express();
+app.use(express.json());
 const fs = require('fs');
-const winston = require('winston');
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'calculate-service' },
-    transports: [
-      //
-      // - Write all logs with importance level of `error` or less to `error.log`
-      // - Write all logs with importance level of `info` or less to `combined.log`
-      //
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
-    ],
-  });
+const userData = fs.readFileSync('users.json');
+const data = JSON.parse(userData);
+
+const admin_JWT_SECRET = "4n7SQn*5b2ldgX6Oj43%xfL$sf4Wd!QP81ERp7CZYoFZeivYmM";
+const user_JWT_SECRET = "ycLMio&sKb&3JEG7zKIb^Dj@!3fHRzp5R20phctTV8q!g^9zOm";
+var tokenKey = ""
+
+  // Authenticating with Admin credentials and other User credentials
+  app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    console.log(`${username} is trying to login ..`);
+    const isUserPresent = data.some(obj => obj.name === username && obj.password === password);
   
-  //
-  // If we're not in production then log to the `console` with the format:
-  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-  //
-  if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-      format: winston.format.simple(),
-    }));
-  }
-const add= (n1,n2) => {
-    return n1+n2;
-}
-const subtract = (n1,n2) => {
-    return n1-n2;
-}
-const multiply = (n1,n2) => {
-    return n1*n2;
-}
-const divide = (n1,n2) => {
-    return n1/n2;
-}
-app.get("/add", (req,res)=>{
-    try{
-    const n1= parseFloat(req.query.n1);
-    const n2=parseFloat(req.query.n2);
-    if(isNaN(n1)) {
-        logger.error("n1 is incorrectly defined");
-        throw new Error("n1 incorrectly defined");
+    if (isUserPresent) {
+      tokenKey = username === "admin"? admin_JWT_SECRET: user_JWT_SECRET;
+      return res.json({
+        token: jwt.sign({ user: username }, tokenKey),
+      });
+    }  
+    return res
+      .status(401)
+      .json({ message: "The username and password your provided are invalid" });
+  });
+
+  // Authorization using jwt.verify
+  app.get("/super-secure-resource", (req, res) => {
+    if (!req.headers.authorization) {
+      return res.status(401).json({ error: "Not Authorized" });
     }
-    if(isNaN(n2)) {
-        logger.error("n2 is incorrectly defined");
-        throw new Error("n2 incorrectly defined");
+    // Bearer <token>>
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(" ")[1];
+    try {
+      // Verify the token is valid
+      const { user } = jwt.verify(token, tokenKey);
+      return res.status(200).json({
+        message: `Congrats ${user}! You can now accesss the super secret resource`,
+      });
+    } catch (error) {
+      return res.status(401).json({ error: "Not Authorized" });
     }
-    
-    if (n1 === NaN || n2 === NaN) {
-        console.log()
-        throw new Error("Parsing Error");
-    }
-    logger.info('Parameters '+n1+' and '+n2+' received for addition');
-    const result = add(n1,n2);
-    url = "/add"
-    res.status(200).json({statuscocde:200, data: result }); 
-    } catch(error) { 
-        console.error(error)
-        res.status(500).json({statuscocde:500, msg: error.toString() })
-      }
-});
-app.get("/subtract", (req,res)=>{
-    try{
-    const n1= parseFloat(req.query.n1);
-    const n2=parseFloat(req.query.n2);
-    if(isNaN(n1)) {
-        logger.error("n1 is incorrectly defined");
-        throw new Error("n1 incorrectly defined");
-    }
-    if(isNaN(n2)) {
-        logger.error("n2 is incorrectly defined");
-        throw new Error("n2 incorrectly defined");
-    }
-    
-    if (n1 === NaN || n2 === NaN) {
-        console.log()
-        throw new Error("Parsing Error");
-    }
-    logger.info('Parameters '+n1+' and '+n2+' received for subtraction');
-    const result = subtract(n1,n2);
-    res.status(200).json({statuscocde:200, data: result }); 
-    } catch(error) { 
-        console.error(error)
-        res.status(500).json({statuscocde:500, msg: error.toString() })
-      }
-});
-app.get("/multiply", (req,res)=>{
-    try{
-    const n1= parseFloat(req.query.n1);
-    const n2=parseFloat(req.query.n2);
-    if(isNaN(n1)) {
-        logger.error("n1 is incorrectly defined");
-        throw new Error("n1 incorrectly defined");
-    }
-    if(isNaN(n2)) {
-        logger.error("n2 is incorrectly defined");
-        throw new Error("n2 incorrectly defined");
-    }
-    
-    if (n1 === NaN || n2 === NaN) {
-        console.log()
-        throw new Error("Parsing Error");
-    }
-    logger.info('Parameters '+n1+' and '+n2+' received for multiplication');
-    const result = multiply(n1,n2);
-    res.status(200).json({statuscocde:200, data: result }); 
-    } catch(error) { 
-        console.error(error)
-        res.status(500).json({statuscocde:500, msg: error.toString() })
-      }
-});
-app.get("/divide", (req,res)=>{
-    try{
-    const n1= parseFloat(req.query.n1);
-    const n2=parseFloat(req.query.n2);
-    if(isNaN(n1)) {
-        logger.error("n1 is incorrectly defined");
-        throw new Error("n1 incorrectly defined");
-    }
-    if(isNaN(n2)) {
-        logger.error("n2 is incorrectly defined");
-        throw new Error("n2 incorrectly defined");
-    }
-    
-    if (n1 === NaN || n2 === NaN) {
-        console.log()
-        throw new Error("Parsing Error");
-    }
-    logger.info('Parameters '+n1+' and '+n2+' received for division');
-    const result = divide(n1,n2);
-    res.status(200).json({statuscocde:200, data: result }); 
-    } catch(error) { 
-        console.error(error)
-        res.status(500).json({statuscocde:500, msg: error.toString() })
-      }
-});
+  });
+
 const port=3040;
 app.listen(port,()=> {
     console.log("hello i'm listening to port " +port);
